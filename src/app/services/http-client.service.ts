bf19c7f575d73c8786fc55872ec971f776b16b6f
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-04-18 10:58:50 +0200

    refactor: Update HttpClientService constructor to use private keyword

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor(http: HttpClient) {
-    this.http = http;
-  }
-
-  http: HttpClient;
+  constructor(private http: HttpClient) {}
 
   get(url: string) : Observable<any> {
     return this.http.get(url);
