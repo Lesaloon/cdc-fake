d48783157d2383191bd9e9bf4b4ec006bba017af
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2024-04-18 10:59:19 +0200

    chore: Add user data to user-grid component

--- src/app/components/user-grid/user-grid.component.ts
+++ src/app/components/user-grid/user-grid.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HttpClientService } from '../../services/http-client.service';
+import { User } from '../../interfaces/user';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-user-grid',
   templateUrl: './user-grid.component.html',
   styleUrls: ['./user-grid.component.css']
 })
 export class UserGridComponent {
+  users: User[] = [];
+  constructor(private httpClientService: HttpClientService, private apiService: ApiService) { }
 
+  ngOnInit() {
+    this.apiService.getAllUsers().subscribe((users: User[]) => {
+      this.users = users;
+    });
+  }
 }
